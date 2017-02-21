89aa722c2ac3f002e9f0108d99243b6baf46b9cc
Meng Fanze <iFanze@outlook.com>
2017-02-21 23:52:17 +0800

    fix

--- weibo.py
+++ weibo.py
@@ -23,10 +23,12 @@ try:
 except ImportError:
     from urllib.request import urlopen
 
+
 class APIError(BaseException):
     '''
     raise APIError if receiving json message indicating failure.
     '''
+
     def __init__(self, error_code, error, request):
         self.error_code = error_code
         self.error = error
@@ -35,6 +37,7 @@ class APIError(BaseException):
 
     def __str__(self):
         return 'APIError: %s: %s, request: %s' % (self.error_code, self.error, self.request)
+
 
 def _parse_json(s):
     ' parse str into JsonDict '
@@ -45,7 +48,9 @@ def _parse_json(s):
         for k, v in pairs.items():
             o[str(k)] = v
         return o
+
     return json.loads(s, object_hook=_obj_hook)
+
 
 class JsonDict(dict):
     ' general json object that allows attributes to be bound to and also behaves like a dict '
@@ -58,6 +63,7 @@ class JsonDict(dict):
 
     def __setattr__(self, attr, value):
         self[attr] = value
+
 
 def _encode_params(**kw):
     '''
@@ -82,6 +88,7 @@ def _encode_params(**kw):
             args.append('%s=%s' % (k, urllib.parse.quote(qv)))
     return '&'.join(args)
 
+
 def _encode_multipart(**kw):
     ' build a multipart/form-data body with randomly generated boundary '
     boundary = '----------%s' % hex(int(time.time() * 1000))
@@ -102,31 +109,37 @@ def _encode_multipart(**kw):
     data.append('--%s--\r\n' % boundary)
     return '\r\n'.join(data), boundary
 
+
 def _guess_content_type(url):
     n = url.rfind('.')
-    if n==(-1):
+    if n == (-1):
         return 'application/octet-stream'
     ext = url[n:]
     return mimetypes.types_map.get(ext, 'application/octet-stream')
+
 
 _HTTP_GET = 0
 _HTTP_POST = 1
 _HTTP_UPLOAD = 2
 
+
 def _http_get(url, authorization=None, **kw):
     logging.info('GET %s' % url)
     return _http_call(url, _HTTP_GET, authorization, **kw)
+
 
 def _http_post(url, authorization=None, **kw):
     logging.info('POST %s' % url)
     return _http_call(url, _HTTP_POST, authorization, **kw)
 
+
 def _http_upload(url, authorization=None, **kw):
     logging.info('MULTIPART POST %s' % url)
     return _http_call(url, _HTTP_UPLOAD, authorization, **kw)
 
+
 def _read_body(obj):
-    using_gzip = obj.headers.get('Content-Encoding', '')=='gzip'
+    using_gzip = obj.headers.get('Content-Encoding', '') == 'gzip'
     body = obj.read()
     if using_gzip:
         gzipper = gzip.GzipFile(fileobj=StringIO(body))
@@ -135,13 +148,14 @@ def _read_body(obj):
         return fcontent
     return body
 
+
 def _http_call(the_url, method, authorization, **kw):
     '''
     send an http request and return a json object if no error occurred.
     '''
     params = None
     boundary = None
-    if method==_HTTP_UPLOAD:
+    if method == _HTTP_UPLOAD:
         # fix sina upload url:
         the_url = the_url.replace('https://api.', 'https://upload.api.')
         params, boundary = _encode_multipart(**kw)
@@ -150,22 +164,24 @@ def _http_call(the_url, method, authorization, **kw):
         if '/remind/' in the_url:
             # fix sina remind api:
             the_url = the_url.replace('https://api.', 'https://rm.api.')
-    http_url = '%s?%s' % (the_url, params) if method==_HTTP_GET else the_url
-    http_body = None if method==_HTTP_GET else params
+    http_url = '%s?%s' % (the_url, params) if method == _HTTP_GET else the_url
+    http_body = None if method == _HTTP_GET else params
 
-    logging.info("data: %s" % http_body)
+    logging.info("param: %s" % http_body)
 
-    http_body = http_body.encode("utf-8")
+    if http_body:
+        http_body = http_body.encode("utf-8")
 
     req = urllib.request.Request(http_url, data=http_body)
-    req.add_header('Accept-Encoding', 'gzip')
+    # req.add_header('Accept-Encoding', 'gzip')
     if authorization:
         req.add_header('Authorization', 'OAuth2 %s' % authorization)
     if boundary:
         req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
     try:
-        resp = urllib.request.urlopen(req, timeout=5).read().decode('utf-8')
-        logging.info(resp)
+        resp = urllib.request.urlopen(req, timeout=5).read()
+        logging.info("resp: %s" % resp)
+        resp = resp.decode('utf-8')
         # body = _read_body(resp)
         body = resp
         r = _parse_json(body)
@@ -182,8 +198,8 @@ def _http_call(the_url, method, authorization, **kw):
             raise APIError(r.error_code, r.get('error', ''), r.get('request', ''))
         raise e
 
-class HttpObject(object):
 
+class HttpObject(object):
     def __init__(self, client, method):
         self.client = client
         self.method = method
@@ -192,14 +208,19 @@ class HttpObject(object):
         def wrap(**kw):
             if self.client.is_expires():
                 raise APIError('21327', 'expired_token', attr)
-            return _http_call('%s%s.json' % (self.client.api_url, attr.replace('__', '/')), self.method, self.client.access_token, **kw)
+            return _http_call('%s%s.json' % (self.client.api_url, attr.replace('__', '/')), self.method,
+                              self.client.access_token, **kw)
+
         return wrap
+
 
 class APIClient(object):
     '''
     API client using synchronized invocation.
     '''
-    def __init__(self, app_key, app_secret, redirect_uri=None, response_type='code', domain='api.weibo.com', version='2'):
+
+    def __init__(self, app_key, app_secret, redirect_uri=None, response_type='code', domain='api.weibo.com',
+                 version='2'):
         self.client_id = str(app_key)
         self.client_secret = str(app_secret)
         self.redirect_uri = redirect_uri
@@ -233,7 +254,7 @@ class APIClient(object):
         if data['algorithm'] != u'HMAC-SHA256':
             return None
         expected_sig = hmac.new(self.client_secret, enc_payload, hashlib.sha256).digest();
-        if expected_sig==sig:
+        if expected_sig == sig:
             data.user_id = data.uid = data.get('user_id', None)
             data.access_token = data.get('oauth_token', None)
             expires = data.get('expires', None)
@@ -255,10 +276,10 @@ class APIClient(object):
             raise APIError('21305', 'Parameter absent: redirect_uri', 'OAuth2 request')
         response_type = kw.pop('response_type', 'code')
         return '%s%s?%s' % (self.auth_url, 'authorize', \
-                _encode_params(client_id = self.client_id, \
-                        response_type = response_type, \
-                        redirect_uri = redirect, **kw))
-                        
+                            _encode_params(client_id=self.client_id, \
+                                           response_type=response_type, \
+                                           redirect_uri=redirect, **kw))
+
     def _parse_access_token(self, r):
         '''
         new:return access token as a JsonDict: {"access_token":"your-access-token","expires_in":12345678,"uid":1234}, expires_in is represented using standard unix-epoch-time
@@ -271,25 +292,25 @@ class APIClient(object):
             if rtime < expires:
                 expires = rtime
         return JsonDict(access_token=r.access_token, expires=expires, expires_in=expires, uid=r.get('uid', None))
-        
+
     def request_access_token(self, code, redirect_uri=None):
         redirect = redirect_uri if redirect_uri else self.redirect_uri
         if not redirect:
             raise APIError('21305', 'Parameter absent: redirect_uri', 'OAuth2 request')
         r = _http_post('%s%s' % (self.auth_url, 'access_token'), \
-                client_id = self.client_id, \
-                client_secret = self.client_secret, \
-                redirect_uri = redirect, \
-                code = code, grant_type = 'authorization_code')
+                       client_id=self.client_id, \
+                       client_secret=self.client_secret, \
+                       redirect_uri=redirect, \
+                       code=code, grant_type='authorization_code')
         return self._parse_access_token(r)
 
     def refresh_token(self, refresh_token):
         req_str = '%s%s' % (self.auth_url, 'access_token')
         r = _http_post(req_str, \
-            client_id = self.client_id, \
-            client_secret = self.client_secret, \
-            refresh_token = refresh_token, \
-            grant_type = 'refresh_token')
+                       client_id=self.client_id, \
+                       client_secret=self.client_secret, \
+                       refresh_token=refresh_token, \
+                       grant_type='refresh_token')
         return self._parse_access_token(r)
 
     def is_expires(self):
@@ -300,10 +321,11 @@ class APIClient(object):
             return getattr(self.get, attr)
         return _Callable(self, attr)
 
-_METHOD_MAP = { 'GET': _HTTP_GET, 'POST': _HTTP_POST, 'UPLOAD': _HTTP_UPLOAD }
+
+_METHOD_MAP = {'GET': _HTTP_GET, 'POST': _HTTP_POST, 'UPLOAD': _HTTP_UPLOAD}
+
 
 class _Executable(object):
-
     def __init__(self, client, method, path):
         self._client = client
         self._method = method
@@ -311,7 +333,7 @@ class _Executable(object):
 
     def __call__(self, **kw):
         method = _METHOD_MAP[self._method]
-        if method==_HTTP_POST and 'pic' in kw:
+        if method == _HTTP_POST and 'pic' in kw:
             method = _HTTP_UPLOAD
         return _http_call('%s%s.json' % (self._client.api_url, self._path), method, self._client.access_token, **kw)
 
@@ -320,16 +342,16 @@ class _Executable(object):
 
     __repr__ = __str__
 
-class _Callable(object):
 
+class _Callable(object):
     def __init__(self, client, name):
         self._client = client
         self._name = name
 
     def __getattr__(self, attr):
-        if attr=='get':
+        if attr == 'get':
             return _Executable(self._client, 'GET', self._name)
-        if attr=='post':
+        if attr == 'post':
             return _Executable(self._client, 'POST', self._name)
         name = '%s/%s' % (self._name, attr)
         return _Callable(self._client, name)
@@ -339,6 +361,8 @@ class _Callable(object):
 
     __repr__ = __str__
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
